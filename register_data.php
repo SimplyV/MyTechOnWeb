c823c1b54c0e11f69810dc8e5988804a21311287
SimplyV <gillian@mfmdigital.com>
2022-04-27 13:02:18 +0200

    Added a lot of functionalities ( start database )

--- register_data.php
+++ register_data.php
@@ -2,29 +2,32 @@
 	
 	session_start();
 
-	if($_POST['password'] != $_POST['passwordconf']){
-		header('Location: register.php');
-		$_SESSION['passwordconferror'] = true;
-	}
-	else{
+	include 'functions.php';$
+	include 'template/parts/db.php';
+
+	if($_POST['password'] == $_POST['passwordconf']){
+		$_SESSION['pseudo'] = $_POST['pseudo'];
 		$_SESSION['lastname'] = $_POST['lastname'];
 		$_SESSION['firstname'] = $_POST['firstname'];
 		$_SESSION['mail'] = $_POST['email'];
+		$allowregister = true;
+	} 
+	else{
+		$allowregister = false;
+		$_SESSION['passwordconferror'] = true;
+		header('Location: register');		
+	}		
+	if($allowregister){
+		$req = $bdd->prepare('INSERT INTO users(firstname, lastname, pseudo, email, passworduser) VALUES(:firstname, :lastname, :pseudo, :email, :passworduser)');
+		$req->execute(array(
+			'firstname' => $_POST['firstname'],
+			'lastname' => $_POST['lastname'],
+			'pseudo' => $_POST['pseudo'],
+			'email' => $_POST['email'],
+			'passworduser' => $_POST['password']
+		)); 
 
-	try{
-		$bdd = new PDO('mysql:host=localhost;dbname=MyTechOnWeb;charset=utf8', 'root', 'root');
+		$_SESSION['passwordconferror'] = false;
+		header('Location: login');
 	}
-
-	catch(Exception $e){
-		die('Erreur : '.$e->getMessage());
-	}
-
-	$bdd->exec('INSERT into users(firstname, lastname, email, password)VALUES(\''.$_SESSION['firstname'].'\',\''. $_SESSION['lastname'].'\', \''.$_SESSION['mail'].'\', \''.$_POST['password'].'\')');
-	
-	$_SESSION['passwordconferror'] = false;
-	header('Location: index.php');
-	
-	}
-
-	
 ?>
